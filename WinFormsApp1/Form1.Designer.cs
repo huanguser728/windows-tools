02bddd523e8fce6d1779de74419ff006bb558e3b
bdd0c51d <11401177+fdfsfsfsdfs@user.noreply.gitee.com>
2023-03-18 10:42:07 +0800

    添加项目文件。

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 20);
+            label1.TabIndex = 0;
+            label1.Text = "关机操作";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 41);
+            label2.Name = "label2";
+            label2.Size = new Size(114, 20);
+            label2.TabIndex = 1;
+            label2.Text = "定时关机（秒）";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(121, 39);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(89, 27);
+            numericUpDown1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 72);
+            button1.Name = "button1";
+            button1.Size = new Size(103, 38);
+            button1.TabIndex = 3;
+            button1.Text = "立刻关机";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(121, 72);
+            button2.Name = "button2";
+            button2.Size = new Size(89, 38);
+            button2.TabIndex = 4;
+            button2.Text = "定时关机";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(240, 9);
+            button3.Name = "button3";
+            button3.Size = new Size(101, 38);
+            button3.TabIndex = 5;
+            button3.Text = "ip设置";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(347, 9);
+            button4.Name = "button4";
+            button4.Size = new Size(101, 38);
+            button4.TabIndex = 6;
+            button4.Text = "注销";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(454, 9);
+            button5.Name = "button5";
+            button5.Size = new Size(101, 38);
+            button5.TabIndex = 7;
+            button5.Text = "打开注册表";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(561, 9);
+            button6.Name = "button6";
+            button6.Size = new Size(98, 38);
+            button6.TabIndex = 8;
+            button6.Text = "打开服务";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(665, 9);
+            button7.Name = "button7";
+            button7.Size = new Size(98, 38);
+            button7.TabIndex = 9;
+            button7.Text = "打开cmd";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(240, 50);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 20);
+            label3.TabIndex = 10;
+            label3.Text = "运行命令";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(240, 72);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(315, 27);
+            textBox1.TabIndex = 11;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(561, 64);
+            button8.Name = "button8";
+            button8.Size = new Size(98, 38);
+            button8.TabIndex = 12;
+            button8.Text = "运行！";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(665, 64);
+            button9.Name = "button9";
+            button9.Size = new Size(98, 38);
+            button9.TabIndex = 13;
+            button9.Text = "关闭wdf";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(12, 116);
+            button10.Name = "button10";
+            button10.Size = new Size(103, 46);
+            button10.TabIndex = 14;
+            button10.Text = "更改uac";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(121, 116);
+            button11.Name = "button11";
+            button11.Size = new Size(108, 46);
+            button11.TabIndex = 15;
+            button11.Text = "Internet选项";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(240, 116);
+            button12.Name = "button12";
+            button12.Size = new Size(103, 46);
+            button12.TabIndex = 16;
+            button12.Text = "系统属性";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(347, 116);
+            button13.Name = "button13";
+            button13.Size = new Size(103, 46);
+            button13.TabIndex = 17;
+            button13.Text = "系统版本";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button13_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 186);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "windows小工具";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private NumericUpDown numericUpDown1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Label label3;
+        private TextBox textBox1;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,195 @@
+using System;
+using System.IO;
+using System.ComponentModel.Design.Serialization;
+
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        Process h = new Process();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            h.StartInfo.FileName = "cmd";
+            h.StartInfo.RedirectStandardOutput = true;
+            h.StartInfo.UseShellExecute = false;
+            h.StartInfo.RedirectStandardInput = true;
+            h.StartInfo.CreateNoWindow = true;
+            h.StartInfo.RedirectStandardOutput = true;
+            h.Start();
+            h.StandardInput.WriteLine("shutdown -s -t 0");
+            h.StandardInput.WriteLine("exit");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sb = numericUpDown1.Value.ToString();
+            h.StartInfo.FileName = "cmd";
+            h.StartInfo.RedirectStandardOutput = true;
+            h.StartInfo.UseShellExecute = false;
+            h.StartInfo.RedirectStandardInput = true;
+            h.StartInfo.CreateNoWindow = true;
+            h.StartInfo.RedirectStandardOutput = true;
+            h.Start();
+            h.StandardInput.WriteLine("shutdown -s -t" + " " + sb);
+            h.StandardInput.WriteLine("exit");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sb = " ";
+            h.StartInfo.FileName = "cmd";
+            h.StartInfo.RedirectStandardOutput = true;
+            h.StartInfo.UseShellExecute = false;
+            h.StartInfo.RedirectStandardInput = true;
+            h.StartInfo.CreateNoWindow = true;
+            h.StartInfo.RedirectStandardOutput = true;
+            h.Start();
+            h.StandardInput.WriteLine("ipconfig&exit");
+            h.StandardInput.AutoFlush = true;
+            sb = h.StandardOutput.ReadToEnd();
+            MessageBox.Show(sb);
+
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            h.StartInfo.FileName = "cmd";
+            h.StartInfo.RedirectStandardOutput = true;
+            h.StartInfo.UseShellExecute = false;
+            h.StartInfo.RedirectStandardInput = true;
+            h.StartInfo.CreateNoWindow = true;
+            h.StartInfo.RedirectStandardOutput = true;
+            h.Start();
+            h.StandardInput.WriteLine("logoff&exit");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            h.StartInfo.FileName = "regedit";
+            h.StartInfo.RedirectStandardOutput = true;
+            h.StartInfo.UseShellExecute = false;
+            h.StartInfo.RedirectStandardInput = true;
+            h.StartInfo.CreateNoWindow = true;
+            h.StartInfo.RedirectStandardOutput = true;
+            h.Start();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            h.StartInfo.FileName = "cmd";
+            h.StartInfo.RedirectStandardOutput = true;
+            h.StartInfo.UseShellExecute = false;
+            h.StartInfo.RedirectStandardInput = true;
+            h.StartInfo.CreateNoWindow = true;
+            h.StartInfo.RedirectStandardOutput = true;
+            h.Start();
+            h.StandardInput.WriteLine("services.msc&exit");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            h.StartInfo.FileName = "cmd";
+            h.StartInfo.RedirectStandardOutput = true;
+            h.StartInfo.UseShellExecute = false;
+            h.StartInfo.RedirectStandardInput = true;
+            h.StartInfo.CreateNoWindow = true;
+            h.StartInfo.RedirectStandardOutput = true;
+            h.Start();
+            h.StandardInput.WriteLine("start cmd & exit");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string into = "cmd";
+            string ss = " ";
+            h.StartInfo.FileName = "cmd";
+            h.StartInfo.RedirectStandardOutput = true;
+            h.StartInfo.UseShellExecute = false;
+            h.StartInfo.RedirectStandardInput = true;
+            h.StartInfo.CreateNoWindow = true;
+            h.StartInfo.RedirectStandardOutput = true;
+            h.Start();
+            into = textBox1.Text;
+            h.StandardInput.WriteLine(into + "&exit");
+            ss = h.StandardOutput.ReadToEnd();
+            MessageBox.Show(ss);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            h.StartInfo.FileName = "cmd";
+            h.StartInfo.RedirectStandardOutput = true;
+            h.StartInfo.UseShellExecute = false;
+            h.StartInfo.RedirectStandardInput = true;
+            h.StartInfo.CreateNoWindow = true;
+            h.StartInfo.RedirectStandardOutput = true;
+            h.Start();
+            h.StandardInput.WriteLine("reg add \"HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\SecurityHealthService\" /v Start /d 3 /t REG_DWORD /f & exit");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            h.StartInfo.FileName = "UserAccountControlSettings.exe";
+            h.StartInfo.RedirectStandardOutput = true;
+            h.StartInfo.UseShellExecute = false;
+            h.StartInfo.RedirectStandardInput = true;
+            h.StartInfo.CreateNoWindow = true;
+            h.StartInfo.RedirectStandardOutput = true;
+            h.Start();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+            h.StartInfo.FileName = "cmd";
+            h.StartInfo.RedirectStandardOutput = true;
+            h.StartInfo.UseShellExecute = false;
+            h.StartInfo.RedirectStandardInput = true;
+            h.StartInfo.CreateNoWindow = true;
+            h.StartInfo.RedirectStandardOutput = true;
+            h.Start();
+            h.StandardInput.WriteLine("inetcpl.cpl & exit");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+            h.StartInfo.FileName = "cmd";
+            h.StartInfo.RedirectStandardOutput = true;
+            h.StartInfo.UseShellExecute = false;
+            h.StartInfo.RedirectStandardInput = true;
+            h.StartInfo.CreateNoWindow = true;
+            h.StartInfo.RedirectStandardOutput = true;
+            h.Start();
+            h.StandardInput.WriteLine("control.exe system & exit");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            h.StartInfo.FileName = "winver";
+            h.StartInfo.RedirectStandardOutput = true;
+            h.StartInfo.UseShellExecute = false;
+            h.StartInfo.RedirectStandardInput = true;
+            h.StartInfo.CreateNoWindow = true;
+            h.StartInfo.RedirectStandardOutput = true;
+            h.Start();
+        }
+    }
+}
